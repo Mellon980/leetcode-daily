25d5b4ff20307b6042cdcd4451f48e0a0e59e052
Mellon980 <343963293@qq.com>
2026-03-09 20:58:59 +0800

    2026-03-09 整数转罗马数字

---
+++ 2026/03/20260309-Integer to Roman.java
@@ -0,0 +1,11 @@
+class Solution {
+        private static final String[][] R = new String[][]{
+        {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}, // 个位
+        {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}, // 十位
+        {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}, // 百位
+        {"", "M", "MM", "MMM"}, // 千位
+    };
+    public String intToRoman(int num) {
+         return R[3][num / 1000] + R[2][num / 100 % 10] + R[1][num / 10 % 10] + R[0][num % 10];
+    }
+}
