f44da9e2a3514dde7af9e428dbb4118a1d46a696
Mellon980 <343963293@qq.com>
2026-03-24 20:44:40 +0800

    2026-03-24 赎金信

---
+++ 2026/03/20260324-Ransom notes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+      if(ransomNote.length()>magazine.length()){
+        return false;
+      }
+      int []cnt=new int [26];
+      for(char c:magazine.toCharArray()){
+        cnt[c-'a']++;
+      }
+      for(char c:ransomNote.toCharArray()){
+        if(--cnt[c-'a']<0){
+            return false;
+        }
+      }
+      return true;
+    }
+}
