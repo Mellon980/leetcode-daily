9ed8d2869c74b82838e0d786a5eeae9d5d9700ab
Mellon980 <343963293@qq.com>
2026-03-27 19:32:37 +0800

    2026-03-27 有效的字母异位词

--- 2026/03/20260327-Valid Alphabetical Ectomy.java
+++ 2026/03/20260327-Valid Alphabetical Ectomy.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isAnagram(String s, String t) {
        int []cntS=new int [26];
        for(char c:s.toCharArray()){
-        cntS[c-'a']++;
+        cntS[c-97]++;
        } 
        int []cntT=new int [26];
        for(char c:t.toCharArray()){
-        cntT[c-'a']++;
+        cntT[c-97]++;
        }
        return Arrays.equals(cntS,cntT);
     }
